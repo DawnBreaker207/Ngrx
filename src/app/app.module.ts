d76ac8a2651e3a7540579c23d759d829839ed7ba
DawnBreaker <tunganhngo207@gmail.com>
2025-01-17 17:25:55 +0700

    fix: fix module bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,53 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {isDevMode, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { isDevMode, NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
-import {MatListModule} from '@angular/material/list';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
-import {RouterModule, Routes} from '@angular/router';
-import {AuthModule} from './auth/auth.module';
-import {StoreModule} from '@ngrx/store';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {environment} from '../environments/environment';
-import {RouterState, StoreRouterConnectingModule} from '@ngrx/router-store';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+// import {RouterState, StoreRouterConnectingModule} from '@ngrx/router-store';
 
-import {EffectsModule} from '@ngrx/effects';
-import {EntityDataModule} from '@ngrx/data';
+// import {EffectsModule} from '@ngrx/effects';
+// import {EntityDataModule} from '@ngrx/data';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AppRoutingModule } from './app-routing.module';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { reducers, metaReducers } from './reducers';
 
-
-const routes: Routes = [
-  {
-    path: 'courses',
-    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
-  },
-  {
-    path: '**',
-    redirectTo: '/'
-  }
-];
-
-
-
-@NgModule({ declarations: [
-    AppComponent
-  ],
-  bootstrap: [AppComponent], imports: [BrowserModule,
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
     MatMenuModule,
     MatIconModule,
     MatSidenavModule,
     MatProgressSpinnerModule,
     MatListModule,
     MatToolbarModule,
-    StoreModule.forRoot({}, {}),
+    AuthModule.forRoot(),
+    StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-    AuthModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class AppModule {
-}
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
+export class AppModule {}
