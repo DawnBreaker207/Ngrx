88e8b3eb0cd0eb366d8358e563fde80dd2eeb3f7
DawnBreaker <tunganhngo207@gmail.com>
2025-01-17 19:49:27 +0700

    feat: update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { isDevMode, NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
-
-import { RouterModule, Routes } from '@angular/router';
-import { AuthModule } from './auth/auth.module';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-// import {RouterState, StoreRouterConnectingModule} from '@ngrx/router-store';
-
-// import {EffectsModule} from '@ngrx/effects';
-// import {EntityDataModule} from '@ngrx/data';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { AppRoutingModule } from './app-routing.module';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { reducers, metaReducers } from './reducers';
+import {BrowserModule} from '@angular/platform-browser';
+import {isDevMode, NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {provideHttpClient, withInterceptorsFromDi,} from '@angular/common/http';
+import {AuthModule} from './auth/auth.module';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {EffectsModule} from '@ngrx/effects';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {AppRoutingModule} from './app-routing.module';
+import {metaReducers, reducers} from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
@@ -43,11 +31,12 @@ import { reducers, metaReducers } from './reducers';
     MatListModule,
     MatToolbarModule,
     AuthModule.forRoot(),
-    StoreModule.forRoot(reducers, { metaReducers }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: !isDevMode()}),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  providers: [provideHttpClient(withInterceptorsFromDi()),],
 })
-export class AppModule {}
+export class AppModule {
+}
