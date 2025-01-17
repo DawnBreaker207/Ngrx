fea98f05b4ba72e388173a39cba11c43be7ce096
DawnBreaker <tunganhngo207@gmail.com>
2025-01-17 15:33:18 +0700

    feat: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,55 @@
-import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {isDevMode, NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
-@NgModule({
-  declarations: [
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+import {RouterModule, Routes} from '@angular/router';
+import {AuthModule} from './auth/auth.module';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {RouterState, StoreRouterConnectingModule} from '@ngrx/router-store';
+
+import {EffectsModule} from '@ngrx/effects';
+import {EntityDataModule} from '@ngrx/data';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
+const routes: Routes = [
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
+
+
+
+@NgModule({ declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
+  bootstrap: [AppComponent], imports: [BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatMenuModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatToolbarModule,
     StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    AuthModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
+export class AppModule {
+}
