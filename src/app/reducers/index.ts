96108d9e4ba79451d4a8f9010e3ee82bd31ad825
DawnBreaker <tunganhngo207@gmail.com>
2025-01-17 17:26:33 +0700

    feat: add new reducers

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,28 @@
+import { routerReducer, RouterReducerState } from '@ngrx/router-store';
+import {
+  ActionReducer,
+  ActionReducerMap,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment.development';
+
+export interface AppState {
+  router: RouterReducerState;
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer,
+};
+
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state before: ', state);
+    console.log('action', action);
+
+    return reducer(state, action);
+  };
+}
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+  ? [logger]
+  : [];
