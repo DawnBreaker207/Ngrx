a307d11bfe85c1233f91f876138d75a869969a5f
DawnBreaker <tunganhngo207@gmail.com>
2025-01-17 17:27:01 +0700

    feat: update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {Observable} from "rxjs";
-import {map} from 'rxjs/operators';
-import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: false
+  styleUrls: ['./app.component.scss'],
+  standalone: false,
 })
 export class AppComponent implements OnInit {
-
   loading = true;
 
-  constructor(private router: Router) {
-
-  }
+  constructor(private router: Router) {}
 
   ngOnInit() {
-
-    this.router.events.subscribe(event => {
+    this.router.events.subscribe((event) => {
       switch (true) {
         case event instanceof NavigationStart: {
           this.loading = true;
@@ -38,11 +40,7 @@ export class AppComponent implements OnInit {
         }
       }
     });
-
   }
 
-  logout() {
-
-  }
-
+  logout() {}
 }
